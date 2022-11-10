d217281fb01907aeb7d647b39b50b59f240627b8
marzad <maschatz@gmx.net>
2022-11-10 19:04:04 +0100

    Edited and add files

--- src/main/java/de/neuefische/marzad/mucjava22library/model/Book.java
+++ src/main/java/de/neuefische/marzad/mucjava22library/model/Book.java
@@ -3,6 +3,7 @@ package de.neuefische.marzad.mucjava22library.model;
 public record Book(
         String isbn,
         String autor,
-        String title
+        String title,
+        BookType type
 ) {
 }
---
+++ src/main/java/de/neuefische/marzad/mucjava22library/model/BookType.java
@@ -0,0 +1,5 @@
+package de.neuefische.marzad.mucjava22library.model;
+
+public enum BookType {
+    SOFT_COVER, E_BOOK, HARD_COVER, AUDIOBOOK
+}
