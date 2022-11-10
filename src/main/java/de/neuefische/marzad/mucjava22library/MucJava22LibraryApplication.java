6f431564d796a6f6237be9b5b2a18f0701508672
marzad <maschatz@gmx.net>
2022-11-10 15:15:34 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/marzad/mucjava22library/MucJava22LibraryApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.marzad.mucjava22library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucJava22LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MucJava22LibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/marzad/mucjava22library/MucJava22LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.marzad.mucjava22library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MucJava22LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
